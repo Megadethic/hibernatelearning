693aede9d2d6911aa710d089155eef5cff3834e1
pavel.kharitonov <pavel.kharitonov@Intradiem.com>
2022-06-13 14:34:13 +0300

    change hibernate version to 5

--- src/main/java/Country.java
+++ src/main/java/Country.java
@@ -1,10 +1,11 @@
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 
 @Getter
